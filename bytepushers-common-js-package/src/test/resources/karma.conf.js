0c55f902290fef2aafd1b771828035fac098f6d5
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2015-12-26 11:23:00 -0800

    Still trying to get Jasmine, Karma, and RequireJs to play nicely with each other.

--- bytepushers-common-js-package/src/test/resources/karma.conf.js
+++ bytepushers-common-js-package/src/test/resources/karma.conf.js
@@ -1,11 +1,22 @@
 /**
  * Created by Tonté Pouncil on 3/21/15.
  */
+var JASMINE="node_modules/jasmine-core/lib/**/*.js",
+    JASMINE_ADAPTER = "node_modules/karma-jasmine/lib/adapter.js",
+    REQUIRE = "node_modules/requirejs/bin/r.js",
+    REQUIRE_ADAPTER = "node_modules/karma-requirejs/lib/adapter.js",
+    ANGULAR_MOCKS = "node_modules/angular-mocks/angular-mocks.js";
+
 module.exports = function(config) {
     config.set({
         basePath: '../../..',
         frameworks: ['jasmine', 'requirejs'],
         files: [
+            JASMINE,
+            JASMINE_ADAPTER,
+            REQUIRE,
+            REQUIRE_ADAPTER,
+
             {pattern: 'node_modules/requirejs/require.js', included: false},
             {pattern: 'src/main/javascript/*.js', included: false},
             {pattern: 'src/test/javascript/**/*.js', included: false},

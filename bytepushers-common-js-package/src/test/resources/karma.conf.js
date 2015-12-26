11f11d283b5b101553bc003fa162acf6483510c6
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2015-12-26 10:36:19 -0800

    Trying to get Requires JS to work inside Jasmine and Karma tests.

--- bytepushers-common-js-package/src/test/resources/karma.conf.js
+++ bytepushers-common-js-package/src/test/resources/karma.conf.js
@@ -6,10 +6,15 @@ module.exports = function(config) {
         basePath: '../../..',
         frameworks: ['jasmine', 'requirejs'],
         files: [
-            'src/main/javascript/*.js',
-            'src/test/javascript/**/*.js'
+            {pattern: 'node_modules/requirejs/require.js', included: false},
+            {pattern: 'src/main/javascript/*.js', included: false},
+            {pattern: 'src/test/javascript/**/*.js', included: false},
+            'src/test/javascript/test-main.js'
         ],
-        exclude: ['src/main/javascript/com.bytepushers.base.app.js'],
+        exclude: [
+            'src/test/resources/karma.conf*.js',
+            'src/main/javascript/com.bytepushers.base.app.js'
+        ],
         reporters: ['progress'],
         port: 9876,
         logLevel: config.LOG_INFO,

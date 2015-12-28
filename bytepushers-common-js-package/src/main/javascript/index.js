69db584ddb8b707ac12213e81aa9c0ddd3199cd9
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2015-12-27 20:39:46 -0800

    Finally got Jasmine, Karma, and RequireJs to play nicely with each other.

--- bytepushers-common-js-package/src/main/javascript/index.js
+++ bytepushers-common-js-package/src/main/javascript/index.js
@@ -1,5 +1,14 @@
 /**
  * Created by tonte on 5/3/15.
  */
-require('./com.bytepushers.base.app.js');
+require('./com.bytepushers.base.app');
 module.exports = BytePushers;
+/*define(function (require) {
+    var BytePushers = require('src/main/javascript/com.bytepushers.base.app');
+    return BytePushers;
+});*/
+/*define(function() {
+    return {
+        'BytePushers': 'com.bytepushers.base.app'
+    };
+});*/

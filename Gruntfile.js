e0ab93d464558c3cc8480805d010d821665daec3
Jared Ramirez <jared.ramirez@bytepushers.software>
2016-01-31 15:10:01 -0600

    finished configuring grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,11 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            build: ["build"],
+            release: ["dist"]
+        },
         jshint: {
             options: {
                 undef: false,
@@ -10,40 +14,69 @@ module.exports = function(grunt) {
             },
             files: ['Gruntfile.js', 'src/**/*.js']
         },
+        jslint: {
+            javascript: {
+                options: {
+                    edition: 'latest',
+                    errorsOnly: true
+                },
+                src: ['Gruntfile.js', 'src/**/*.js']
+            }
+        },
         karma: {
             unit: {
-                configFile: 'karma.conf.js',
-                autoWatch: true
+                configFile: 'karma.conf.js'
+            }
+        },
+        copy: {
+            main: {
+                files: [{expanded: true, src: ['src/main/javascript/*.js'], dest: 'build/', filter: 'isFile'}]
+            }
+        },
+        jsdoc: {
+            dist: {
+                src: ['build/src/main/javascript/*.js'],
+                options: {
+                    destination: 'dist/doc'
+                }
+            }
+        },
+        uglify: {
+            build_min: {
+                options: {
+                    mangle: true
+                },
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['build/src/main/javascript/*.js']
+                }
+            },
+            build: {
+                options: {
+                    beautify: true
+                },
+                files: {
+                    'dist/<%= pkg.name %>.js': ['build/src/main/javascript/*.js']
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-karma');
+    var target = grunt.option('target') || 'build';
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['jshint']);
-    /*
-     default task
-     run: $ grunt
-    */
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('custom', 'My "custom" task.', function() {
-        grunt.log.writeln('Successfully running my "' + this.name + '" task.');
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('build', ['clean:' + target, 'lint', 'test', 'package']);
 
-        grunt.task.run('bar');
-        grunt.task.run(['foo']);
-
-        if(true){
-            grunt.log.error('There was an error(not!).');
-        }
-    });
-
-    grunt.registerTask('bar', 'My "bar" task.', function() {
-        grunt.log.writeln('BAR.');
-    });
-
-    grunt.registerTask('foo', 'My "foo" task.', function() {
-        grunt.task.requires('bar');
-        grunt.log.writeln('FOO.');
-    });
+    grunt.registerTask('lint', ['jshint', 'jslint']);
+    grunt.registerTask('test', ['karma']);
+    grunt.registerTask('package', ['copy', 'jsdoc', 'uglify']);
 };

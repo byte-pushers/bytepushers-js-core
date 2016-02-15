fb837af1332f0f9cc19d51416669accfb8bfdfcb
Jared Ramirez <jared.ramirez@bytepushers.software>
2016-02-15 17:07:48 -0600

    finished updating requirejs config.

--- bytepushers-common-js-client/app/main.js
+++ bytepushers-common-js-client/app/main.js
@@ -1,14 +1,15 @@
 requirejs.config({
     paths: {
-        "angular" : "../node_modules/angular/angular.min",
-        "uiRouter": "../node_modules/angular-ui-router/release/angular-ui-router.min",
+        'angular' : '../node_modules/angular/angular.min',
+        'uiRouter': '../node_modules/angular-ui-router/release/angular-ui-router.min',
         'domReady': 'bower_components/domReady/domReady',
-        "bytepushers": "jspm_packages/github/byte-pushers/bytepushers-common-js@0.0.4/com.bytepushers.base.app"
+        /*'bytepushers': ['jspm_packages/github/byte-pushers/bytepushers-common-js@0.0.4/com.bytepushers.base.app']*/
+        'bytepushers': 'jspm_packages/github/byte-pushers/bytepushers-common-js@0.0.4/index'
     },
 
     shim: {
-        "angular": {
-            exports: "angular"
+        'angular': {
+            exports: 'angular'
         },
         'uiRouter' : {
             deps : ['angular']

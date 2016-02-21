19c7c0743ed4a605b66a0669c66db6b3a4be0657
Jared Ramirez <jared.ramirez@bytepushers.software>
2016-02-21 12:29:32 -0600

    DateUtility is lint free

--- test-main.js
+++ test-main.js
@@ -25,14 +25,34 @@ require.config({
 
   paths: {
       'bytepushers': 'src/main/javascript/software.bytepushers.base.app',
-      'bytepushersNumberUtility': 'src/main/javascript/software.bytepushers.utils.NumberUtility'
+      'bytepushersErrors': 'src/main/javascript/software.bytepushers.exceptions.Errors',
+      'bytepushersDateConverter': 'src/main/javascript/software.bytepushers.utils.converters.date.DateConverter',
+      'bytepushersDateUtility': 'src/main/javascript/software.bytepushers.utils.DateUtility',
+      'bytepushersDOMUtility': 'src/main/javascript/software.bytepushers.utils.DOMUtility',
+      'bytepushersNumberUtility': 'src/main/javascript/software.bytepushers.utils.NumberUtility',
+      'bytepushersResourceLoader': 'src/main/javascript/software.bytepushers.utils.ResourceLoader'
   },
 
   shim: {
       bytepushers: {
           exports: 'BytePushers'
       },
+      bytepushersErrors: {
+          deps: ['bytepushers']
+      },
+      bytepushersDateConverter: {
+          deps: ['bytepushers']
+      },
+      bytepushersDateUtility: {
+          deps: ['bytepushers']
+      },
+      bytepushersDOMUtility: {
+          deps: ['bytepushers']
+      },
       bytepushersNumberUtility: {
+          deps: ['bytepushers']
+      },
+      bytepushersResourceLoader: {
           deps: ['bytepushers']
       }
   },

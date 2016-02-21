ff4904d44e97719c9dcf71f5960637e85d8389bc
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2016-02-21 11:43:33 -0600

    Re-factored to satisfy JSLint and now this file is lint free.

--- src/main/javascript/software.bytepushers.exceptions.Errors.js
+++ src/main/javascript/software.bytepushers.exceptions.Errors.js
@@ -1,41 +1,42 @@
-(function (window, document, BytePushers) {
-	"use strict";
-	BytePushers = BytePushers || {};
-	BytePushers.exceptions = BytePushers.namespace("software.bytepushers.exceptions");
-	BytePushers.exceptions.InvalidParameterException = function (message) {
-		Error.call(this, message);
-		BytePushers.exceptions.InvalidParameterException.prototype = new Error();
-		this.name = "BytePushers.exceptions.InvalidParameterException";
-		this.message = message;
-	};
-	BytePushers.exceptions.InvalidParameterException.prototype.toString = function() {
-		return this.name + "(" + this.message + ")";
-	};
-	BytePushers.exceptions.NullPointerException = function (message) {
-		Error.call(this, message);
-		BytePushers.exceptions.NullPointerException.prototype = new Error();
-		this.name = "BytePushers.exceptions.NullPointerException";
-		this.message = message;
-	};
-	BytePushers.exceptions.NullPointerException.prototype.toString = function() {
-		return this.name + "(" + this.message + ")";
-	};
-	BytePushers.exceptions.ExpectedArrayIsEmptyException = function (message) {
-		Error.call(this, message);
-		BytePushers.exceptions.ExpectedArrayIsEmptyException.prototype = new Error();
-		this.name = "BytePushers.exceptions.ExpectedArrayIsEmptyException";
-		this.message = message;
-	};
-	BytePushers.exceptions.ExpectedArrayIsEmptyException.prototype.toString = function() {
-		return this.name + "(" + this.message + ")";
-	};
-	BytePushers.exceptions.InvalidDateRangeException = function (message) {
-		Error.call(this, message);
-		BytePushers.exceptions.InvalidDateRangeException.prototype = new Error();
-		this.name = "BytePushers.exceptions.InvalidDateRangeException";
-		this.message = message;
-	};
-	BytePushers.exceptions.InvalidDateRangeException.prototype.toString = function() {
-		return this.name + "(" + this.message + ")";
-	};
-}(window, document, BytePushers));
+/*global BytePushers*/
+(function (BytePushers) {
+    "use strict";
+    BytePushers = BytePushers || {};
+    BytePushers.exceptions = BytePushers.namespace("software.bytepushers.exceptions");
+    BytePushers.exceptions.InvalidParameterException = function (message) {
+        Error.call(this, message);
+        BytePushers.exceptions.InvalidParameterException.prototype = new Error();
+        this.name = "BytePushers.exceptions.InvalidParameterException";
+        this.message = message;
+    };
+    BytePushers.exceptions.InvalidParameterException.prototype.toString = function () {
+        return this.name + "(" + this.message + ")";
+    };
+    BytePushers.exceptions.NullPointerException = function (message) {
+        Error.call(this, message);
+        BytePushers.exceptions.NullPointerException.prototype = new Error();
+        this.name = "BytePushers.exceptions.NullPointerException";
+        this.message = message;
+    };
+    BytePushers.exceptions.NullPointerException.prototype.toString = function () {
+        return this.name + "(" + this.message + ")";
+    };
+    BytePushers.exceptions.ExpectedArrayIsEmptyException = function (message) {
+        Error.call(this, message);
+        BytePushers.exceptions.ExpectedArrayIsEmptyException.prototype = new Error();
+        this.name = "BytePushers.exceptions.ExpectedArrayIsEmptyException";
+        this.message = message;
+    };
+    BytePushers.exceptions.ExpectedArrayIsEmptyException.prototype.toString = function () {
+        return this.name + "(" + this.message + ")";
+    };
+    BytePushers.exceptions.InvalidDateRangeException = function (message) {
+        Error.call(this, message);
+        BytePushers.exceptions.InvalidDateRangeException.prototype = new Error();
+        this.name = "BytePushers.exceptions.InvalidDateRangeException";
+        this.message = message;
+    };
+    BytePushers.exceptions.InvalidDateRangeException.prototype.toString = function () {
+        return this.name + "(" + this.message + ")";
+    };
+}(BytePushers));

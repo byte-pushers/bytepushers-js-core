d3296a58f8ecd1c1791e0c5bd13e4c54614e7631
Jared Ramirez <jared.ramirez@bytepushers.software>
2016-02-28 12:37:23 -0600

    Changed main.js to run successfully.

--- bytepushers-common-js-client/src/main/webapp/main.js
+++ bytepushers-common-js-client/src/main/webapp/main.js
@@ -4,9 +4,9 @@ requirejs.config({
         'uiRouter': 'bower_components/angular-ui-router/release/angular-ui-router.min',
         'domReady': 'bower_components/requirejs-domready/domReady',
         /*The configuration below does not load modules/js files defined within the index.js file*/
-        'bytepushers': 'jspm_packages/github/byte-pushers/bytepushers-common-js@0.0.4/index'
+        /*'bytepushers': 'jspm_packages/github/byte-pushers/bytepushers-common-js@0.0.4/index'*/
         /*The configuration below successfully loads one JS file; Which is com.bytepushers.base.app.js*/
-        /*'bytepushers': 'jspm_packages/github/byte-pushers/bytepushers-common-js@0.0.4/com.bytepushers.base.app'*/
+        'bytepushers': 'jspm_packages/github/byte-pushers/bytepushers-common-js@0.0.4/com.bytepushers.base.app'
     },
 
     shim: {

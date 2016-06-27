d3f853a7a6771221440a33ba314b8464a450670a
Jared Ramirez <jaredramirez@me.com>
2016-06-27 16:58:38 -0500

    added date utility functions

--- src/test/javascript/dateUtilitySpec.js
+++ src/test/javascript/dateUtilitySpec.js
@@ -8,5 +8,17 @@ define(['bytepushers', 'bytepushersDateUtility'], function(BytePushers) {
             var date1 = new Date(97,2,30), date2 = new Date(99, 6, 23);
             expect(BytePushers.DateUtility.date_sort_asc(date1, date2)).toEqual(-1);
         });
+
+        it("validate date string", function(){
+            var date = '11/17/2000';
+            expect(BytePushers.DateUtility.isValidDateString(date)).toEqual(true);
+            date = '11/17/20000';
+            expect(BytePushers.DateUtility.isValidDateString(date)).toEqual(false);
+        });
+
+        it("validate date string", function(){
+            var date = new Date('11/17/2000');
+            expect(BytePushers.DateUtility.formatDateToString(date)).toEqual('11/17/2000');
+        });
     });
 });

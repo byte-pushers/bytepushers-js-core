4ca9e735aa9b62ed3e2147881fbf9e1af22c0552
Tonté Pouncil <pouncilt.developer@gmail.com>
2016-08-01 21:44:17 -0700

    Release v0.0.64

--- src/main/javascript/software.bytepushers.BytePushers.js
+++ src/main/javascript/software.bytepushers.BytePushers.js
@@ -1,4 +1,5 @@
 /*global window, document*/
+/* jshint -W108, -W109 */
 /*jslint bitwise: true, unparam: true, regexp: true*/
 
 (function (window, document) {
@@ -530,6 +531,58 @@
 
         return hasFunction;
     };
+    Object.isFunction = function (target) {
+        var isFunction = false;
+
+        if (Object.isDefined(target)) {
+            if (typeof target === "function") {
+                isFunction = true;
+            }
+        }
+
+        return isFunction;
+    };
+
+    Object.isFunction = function (target) {
+        var isFunction = false;
+
+        if (Object.isDefined(target)) {
+            if (typeof target === "function") {
+                isFunction = true;
+            }
+        }
+
+        return isFunction;
+    };
+
+    Object.isConstructorFunction = function (targetFunction) {
+        var isConstructorFunction = false,
+            isNotFirstLetterUppercase;
+
+        if (Object.isFunction(targetFunction)) {
+            isNotFirstLetterUppercase = !(/^[A-Z]/.test(targetFunction.name));
+            isConstructorFunction = true;
+        } else {
+            throw new BytePushers.exceptions.InvalidParameterException("Function(" + targetFunction + ") is not a Function.");
+        }
+
+        if (isNotFirstLetterUppercase) {
+            throw new BytePushers.exceptions.InvalidParameterException("Fist letter of Function(" + targetFunction + ") name must be capitalized.");
+        }
+
+        return isConstructorFunction;
+    };
+
+    if (Function.prototype.name === undefined) {
+        // Add a custom property to all function values
+        // that actually invokes a method to get the value
+        Object.defineProperty(Function.prototype, 'name', {
+            get: function () {
+                return (/function ([^(]*)/).exec(this)[1];
+            }
+        });
+    }
+
     /* END Object Extensions *
      ****************************************************************************************************/
 
@@ -606,6 +659,44 @@
 
         return theString;
     };
+
+    if (!String.prototype.substringBefore) {
+        /**
+         * Convenience method taht returns the sub-string found before the search criteria.
+         *
+         * @param searchCriteria  Represent a search string to base ending index on.
+         * @returns {*}
+         */
+        String.prototype.substringBefore = function (searchCriteria) {
+            var searchResult = searchCriteria,
+                searchedStringIndex = this.search(searchCriteria);
+
+            if (searchedStringIndex > 0) {
+                searchResult = this.substring(0, searchedStringIndex);
+            }
+
+            return searchResult;
+        };
+    }
+
+    if (!String.prototype.substringAfter) {
+        /**
+         * Convenience method that returns the sub-string found after search criteria.
+         *
+         * @param searchCriteria  Represent a search string to base starting index on.
+         * @returns {*}
+         */
+        String.prototype.substringAfter = function (searchCriteria) {
+            var searchResult = searchCriteria,
+                searchedStringIndex = this.search(searchCriteria);
+
+            if (searchedStringIndex > -1 && searchedStringIndex + searchCriteria.length <= this.length) {
+                searchResult = this.substring(searchedStringIndex + searchCriteria.length, this.length);
+            }
+
+            return searchResult;
+        };
+    }
     /* END String Extensions *****************************************************************************************************/
 
 
--- test-main.js
+++ test-main.js
@@ -1,7 +1,7 @@
 var allTestFiles = [];
 for (var file in window.__karma__.files) {
     if (window.__karma__.files.hasOwnProperty(file)) {
-        if (/Spec\.js$/.test(file)) {
+        if (/[Ss]pec\.js$/.test(file)) {
             allTestFiles.push(file);
         }
     }
@@ -18,7 +18,10 @@ require.config({
       'bytepushersDateUtility': 'src/main/javascript/software.bytepushers.utils.DateUtility',
       'bytepushersDOMUtility': 'src/main/javascript/software.bytepushers.utils.DOMUtility',
       'bytepushersNumberUtility': 'src/main/javascript/software.bytepushers.utils.NumberUtility',
-      'bytepushersResourceLoader': 'src/main/javascript/software.bytepushers.utils.ResourceLoader'
+      'bytepushersResourceLoader': 'src/main/javascript/software.bytepushers.utils.ResourceLoader',
+      'bytepushersReflection': 'src/main/javascript/software.bytepushers.utils.Reflection',
+      'BaseEntity': 'src/test/javascript/support/software.bytepushers.model.BaseEntity',
+      'Person': 'src/test/javascript/support/software.bytepushers.model.Person'
   },
 
   shim: {
@@ -45,6 +48,15 @@ require.config({
       },
       bytepushersResourceLoader: {
           deps: ['bytepushers']
+      },
+      bytepushersReflection: {
+          deps: ['bytepushers']
+      },
+      BaseEntity: {
+          deps: ['bytepushers']
+      },
+      Person: {
+          deps: ['bytepushers', 'BaseEntity']
       }
   },
 
